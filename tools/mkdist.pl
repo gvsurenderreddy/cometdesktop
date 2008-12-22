a71699583ba69c9cd51e60abce7c24cc390a4ae1
david.davis <david.davis@12ea08fa-c998-11dd-a2ab-b939405fd53f>
2008-12-22 22:36:45 +0000

    move INSTALL LICENSE README TODO

--- tools/mkdist.pl
+++ tools/mkdist.pl
@@ -8,10 +8,10 @@ open ( my $fh, "cometdesktop/VERSION" ) or die "VERSION: $!";
 my ( $v ) = <$fh>;
 chomp $v;
 
-my $file = "cometdesktop-${v}.zip";
+my $file = "cometdesktop-v${v}.zip";
 
 system("rm $file") if ( -e $file );
 
-system('zip -r '.$file.' cometdesktop -x@tools/exclude.list -i@tools/include.list');
+system('zip -r '.$file.' cometdesktop -i@tools/include.list -x@tools/exclude.list');
 
 print "\n\ncreated $file\n";
