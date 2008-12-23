e76377560e1b2204db09cc69f487de6918d02d45
david.davis <david.davis@12ea08fa-c998-11dd-a2ab-b939405fd53f>
2008-12-23 19:48:21 +0000

    fix the license header

--- cometdesktop/perl-lib/CometDesktop/Common.pm
+++ cometdesktop/perl-lib/CometDesktop/Common.pm
@@ -1,11 +1,11 @@
 # Comet Desktop
-# # Copyright (c) 2008 - David W Davis, All Rights Reserved
-# # xantus@cometdesktop.com     http://xant.us/
-# # http://code.google.com/p/cometdesktop/
-# # http://cometdesktop.com/
-# #
-# # License: GPL v3
-# # http://code.google.com/p/cometdesktop/wiki/License
+# Copyright (c) 2008 - David W Davis, All Rights Reserved
+# xantus@cometdesktop.com     http://xant.us/
+# http://code.google.com/p/cometdesktop/
+# http://cometdesktop.com/
+# 
+# License: GPL v3
+# http://code.google.com/p/cometdesktop/wiki/License
 
 package CometDesktop::Common;
 
